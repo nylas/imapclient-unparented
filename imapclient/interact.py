0139c2a935a4d6e02fbda369f01891b6fd4ff0f2
Menno Smits <menno@freshfoo.com>
2015-06-30 21:59:57 +1200

    Merged stable into default

---
+++ imapclient/interact.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+# Copyright (c) 2014, Menno Smits
+# Released subject to the New BSD License
+# Please see http://en.wikipedia.org/wiki/BSD_licenses
+
+from __future__ import unicode_literals
+
+
+from getpass import getpass
+from optparse import OptionParser
+
+from .config import parse_config_file, create_client_from_config
+
+def command_line():
+    p = OptionParser()
+    p.add_option('-H', '--host', dest='host', action='store',
+                 help='IMAP host connect to')
+    p.add_option('-u', '--username', dest='username', action='store',
+                 help='Username to login with')
+    p.add_option('-p', '--password', dest='password', action='store',
+                 help='Password to login with')
+    p.add_option('-P', '--port', dest='port', action='store', type=int,
+                 default=None,
+                 help='IMAP port to use (default is 143, or 993 for SSL)')
+    p.add_option('-s', '--ssl', dest='ssl', action='store_true', default=False,
+                 help='Use SSL connection')
+    p.add_option('-f', '--file', dest='file', action='store', default=None,
+                 help='Config file (same as livetest)')
+
+    opts, args = p.parse_args()
+    if args:
+        p.error('unexpected arguments %s' % ' '.join(args))
+
+    if opts.file:
+        if opts.host or opts.username or opts.password or opts.port or opts.ssl:
+            p.error('If -f/--file is given no other options can be used')
+        # Use the options in the config file
+        opts = parse_config_file(opts.file)
+    else:
+        # Get compulsory options if not given on the command line
+        for opt_name in ('host', 'username', 'password'):
+            if not getattr(opts, opt_name):
+                setattr(opts, opt_name, getpass(opt_name + ': '))
+        # Options not supported on the command line
+        opts.oauth = False
+        opts.oauth2 = False
+        opts.stream = False
+    return opts
+
+def main():
+    opts = command_line()
+    print('Connecting...')
+    client = create_client_from_config(opts)
+    print('Connected.')
+    banner = '\nIMAPClient instance is "c"'
+
+    def ipython_011(c):
+        from IPython.frontend.terminal.embed import InteractiveShellEmbed
+        ipshell = InteractiveShellEmbed(banner1=banner)
+        ipshell('')
+
+    def ipython_010(c):
+        from IPython.Shell import IPShellEmbed
+        IPShellEmbed('', banner=banner)()
+
+    def builtin(c):
+        import code
+        code.interact(banner, local=dict(c=c))
+    
+    for shell_attempt in (ipython_011, ipython_010, builtin):
+        try:
+            shell_attempt(client)
+            break
+        except ImportError:
+            pass
+
+if __name__ == '__main__':
+    main()
