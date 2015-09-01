107fc438852ca1f8a9dcec5e48b1f7560e9ecf3a
Menno Smits <menno@freshfoo.com>
2015-09-01 15:04:57 +1200

    Support regex matching in "alternates" live test

---
+++ imapclient/config.py
@@ -0,0 +1,190 @@
+# Copyright (c) 2015, Menno Smits
+# Released subject to the New BSD License
+# Please see http://en.wikipedia.org/wiki/BSD_licenses
+
+from __future__ import unicode_literals
+
+# TODO: can't six do this for us?
+try:
+    from ConfigParser import SafeConfigParser, NoOptionError
+except ImportError:
+    from configparser import SafeConfigParser, NoOptionError
+
+from os import path
+from backports import ssl
+
+from six.moves.urllib.request import urlopen
+from six.moves.urllib.parse import urlencode
+
+import imapclient
+from .tls import create_default_context
+
+try:
+    import json
+except ImportError:
+    json = None
+
+
+def parse_config_file(filename):
+    """Parse INI files containing IMAP connection details.
+
+    Used by livetest.py and interact.py
+    """
+    parser = SafeConfigParser(dict(
+        username=None,
+        password=None,
+        ssl='false',
+        ssl_check_hostname='true',
+        ssl_verify_cert='true',
+        ssl_ca_file=None,
+        timeout=None,
+        starttls='false',
+        stream='false',
+        oauth='false',
+        oauth_token=None,
+        oauth_token_secret=None,
+        oauth_url=None,
+        oauth2='false',
+        oauth2_client_id=None,
+        oauth2_client_secret=None,
+        oauth2_refresh_token=None,
+        expect_failure=None,
+    ))
+    with open(filename, 'r') as fh:
+        parser.readfp(fh)
+
+    conf = _read_config_section(parser, "DEFAULT")
+    if conf.expect_failure:
+        raise ValueError("expect_failure should not be set for the DEFAULT section")
+
+    conf.alternates = {}
+    for section in parser.sections():
+        conf.alternates[section] = _read_config_section(parser, section)
+
+    return conf
+
+
+def _read_config_section(parser, section):
+    get = lambda name: parser.get(section, name)
+    getboolean = lambda name: parser.getboolean(section, name)
+
+    def get_allowing_none(name, typefunc):
+        try:
+            v = parser.get(section, name)
+        except NoOptionError:
+            return None
+        if not v:
+            return None
+        return typefunc(v)
+
+    def getint(name):
+        return get_allowing_none(name, int)
+
+    def getfloat(name):
+        return get_allowing_none(name, float)
+
+    ssl_ca_file = get('ssl_ca_file')
+    if ssl_ca_file:
+        ssl_ca_file = path.expanduser(ssl_ca_file)
+
+    return Bunch(
+        host=get('host'),
+        port=getint('port'),
+        ssl=getboolean('ssl'),
+        starttls=getboolean('starttls'),
+        ssl_check_hostname=getboolean('ssl_check_hostname'),
+        ssl_verify_cert=getboolean('ssl_verify_cert'),
+        ssl_ca_file=ssl_ca_file,
+        timeout=getfloat('timeout'),
+
+        stream=getboolean('stream'),
+
+        username=get('username'),
+        password=get('password'),
+
+        oauth=getboolean('oauth'),
+        oauth_url=get('oauth_url'),
+        oauth_token=get('oauth_token'),
+        oauth_token_secret=get('oauth_token_secret'),
+
+        oauth2=getboolean('oauth2'),
+        oauth2_client_id=get('oauth2_client_id'),
+        oauth2_client_secret=get('oauth2_client_secret'),
+        oauth2_refresh_token=get('oauth2_refresh_token'),
+
+        expect_failure=get('expect_failure')
+    )
+
+
+def refresh_oauth2_token(client_id, client_secret, refresh_token):
+    if not json:
+        raise RuntimeError("livetest OAUTH2 functionality relies on 'json' module")
+    post = dict(client_id=client_id.encode('ascii'),
+                client_secret=client_secret.encode('ascii'),
+                refresh_token=refresh_token.encode('ascii'),
+                grant_type=b'refresh_token')
+    response = urlopen('https://accounts.google.com/o/oauth2/token',
+                       urlencode(post).encode('ascii')).read()
+    return json.loads(response.decode('ascii'))['access_token']
+
+# Tokens are expensive to refresh so use the same one for the duration of the process.
+_oauth2_cache = {}
+
+
+def get_oauth2_token(client_id, client_secret, refresh_token):
+    cache_key = (client_id, client_secret, refresh_token)
+    token = _oauth2_cache.get(cache_key)
+    if token:
+        return token
+    token = refresh_oauth2_token(client_id, client_secret, refresh_token)
+    _oauth2_cache[cache_key] = token
+    return token
+
+
+def create_client_from_config(conf):
+    ssl_context = None
+    if conf.ssl:
+        ssl_context = create_default_context()
+        ssl_context.check_hostname = conf.ssl_check_hostname
+        if not conf.ssl_verify_cert:
+            ssl_context.verify_mode = ssl.CERT_NONE
+        if conf.ssl_ca_file:
+            ssl_context.load_verify_locations(cafile=conf.ssl_ca_file)
+
+    client = imapclient.IMAPClient(conf.host, port=conf.port,
+                                   ssl=conf.ssl,
+                                   ssl_context=ssl_context,
+                                   stream=conf.stream,
+                                   timeout=conf.timeout)
+    try:
+        if conf.starttls:
+            client.starttls()
+
+        if conf.oauth:
+            client.oauth_login(conf.oauth_url,
+                               conf.oauth_token,
+                               conf.oauth_token_secret)
+        elif conf.oauth2:
+            access_token = get_oauth2_token(conf.oauth2_client_id,
+                                            conf.oauth2_client_secret,
+                                            conf.oauth2_refresh_token)
+            client.oauth2_login(conf.username, access_token)
+
+        elif not conf.stream:
+            client.login(conf.username, conf.password)
+        return client
+    except:
+        client.shutdown()
+        raise
+
+
+class Bunch(dict):
+
+    def __getattr__(self, k):
+        try:
+            return self[k]
+        except KeyError:
+            raise AttributeError
+
+    def __setattr__(self, k, v):
+        self[k] = v
---
+++ setup.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2015, Menno Smits
+# Released subject to the New BSD License
+# Please see http://en.wikipedia.org/wiki/BSD_licenses
+
+import sys
+from os import path
+
+# bootstrap setuptools if necessary
+from ez_setup import use_setuptools
+use_setuptools(version="17.1")
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+MAJ_MIN = sys.version_info[:2]
+IS_PY3 = MAJ_MIN >= (3, 0)
+IS_PY_26_OR_OLDER = MAJ_MIN <= (2, 6)
+IS_PY_34_OR_NEWER = MAJ_MIN >= (3, 4)
+
+# Read version info
+version_file = path.join('imapclient', 'version.py')
+info = {}
+if IS_PY3:
+    exec(open(version_file).read(), {}, info)
+else:
+    execfile(version_file, {}, info)
+
+desc = """\
+IMAPClient is an easy-to-use, Pythonic and complete IMAP client library.
+
+Features:
+    * Arguments and return values are natural Python types.
+    * IMAP server responses are fully parsed and readily usable.
+    * IMAP unique message IDs (UIDs) are handled transparently.
+    * Internationalised mailbox names are transparently handled.
+    * Time zones are correctly handled.
+    * Convenience methods are provided for commonly used functionality.
+    * Exceptions are raised when errors occur.
+
+Python versions 2.6, 2.7, 3.3 and 3.4 are officially supported.
+
+IMAPClient includes comprehensive units tests and automated
+functional tests that can be run against a live IMAP server.
+"""
+
+
+class TestDiscoverCommand(TestCommand):
+    """
+    Use unittest2 to discover and run tests
+    """
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        from imapclient.test.util import unittest   # this will import unittest2
+        module = "__main__"
+        if IS_PY_26_OR_OLDER or IS_PY_34_OR_NEWER:
+            module = None
+        unittest.main(argv=['', 'discover'], module=module)
+
+main_deps = [
+    'backports.ssl>=0.0.7',
+    'pyopenssl>=0.15.1',
+    'six',
+    'mock==1.3.0'
+]
+
+setup_deps = main_deps + ['sphinx']
+
+test_deps = []
+if IS_PY_26_OR_OLDER:
+    test_deps.append('unittest2')
+
+setup(name='IMAPClient',
+      version=info['version'],
+      author=info['author'],
+      author_email=info['author_email'],
+      license="http://en.wikipedia.org/wiki/BSD_licenses",
+      url="http://imapclient.freshfoo.com/",
+      download_url='http://freshfoo.com/projects/IMAPClient/IMAPClient-%s.zip' % info['version'],
+      packages=find_packages(),
+      package_data=dict(imapclient=['examples/*.py']),
+      setup_requires=setup_deps,
+      install_requires=main_deps,
+      tests_require=test_deps,
+      description="Easy-to-use, Pythonic and complete IMAP client library",
+      long_description=desc,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Communications :: Email :: Post-Office :: IMAP',
+          'Topic :: Internet',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Networking'],
+      cmdclass={'test': TestDiscoverCommand})
